7f5c2ff9e5c4b5c0c9d7fd37c845f5fe4a54212f
Gouthami492 <69711081+Gouthami492@users.noreply.github.com>
2020-12-20 00:17:35 +0530

    Add files via upload
    Here's a python program which scraps the data i.e top most songs in 2020 on gaana

---
+++ gaaana.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+data=requests.get("https://gaana.com/playlist/arkabanerjee-whxhx-topenglishsongs")
+soup=BeautifulSoup(data.text,"html.parser")
+
+header=['Serial No','Title','Artist(s)']
+songs=[]
+
+artist_tag=soup.find_all("a","sng_c")
+#artist_tag=soup.find_all("li","sng_c")
+title_tag=soup.find_all("div","track_npqitemdetail")
+
+serial_no=[]
+title=[]
+artists=[]
+
+print(header)
+
+for i in range(0,len(title_tag)):
+    serial_no.append(i+1)
+    title.append(title_tag[i].span.text)
+    #artists.append(artist_tag[i]..text)
+    song=[]
+    song.append(serial_no[i])
+    song.append(title[i])
+    #song.append(artists[i])
+    songs.append(song)
+
+for song in songs:
+    print(song)
